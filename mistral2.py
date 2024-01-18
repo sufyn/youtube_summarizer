4199291935ec7da32d38f9acc1b3115ac66dd4fa
sufyaan <97327266+sufyn@users.noreply.github.com>
2024-01-19 00:32:50 +0530

    Update mistral2.py

--- mistral2.py
+++ mistral2.py
@@ -1,49 +1,11 @@
 import streamlit as st
 import os
 
-import torch
-from transformers import BitsAndBytesConfig
-
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
-
-model_id = "mistralai/Mistral-7B-Instruct-v0.1"
-
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-model_4bit = AutoModelForCausalLM.from_pretrained( model_id, device_map="auto",quantization_config=quantization_config, )
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-pipeline = pipeline(
-        "text-generation",
-        model=model_4bit,
-        tokenizer=tokenizer,
-        use_cache=True,
-        device_map="auto",
-        max_length=500,
-        do_sample=True,
-        top_k=5,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        pad_token_id=tokenizer.eos_token_id,
-)
-
-from langchain import HuggingFacePipeline
 from langchain import PromptTemplate, LLMChain
-llm = HuggingFacePipeline(pipeline=pipeline)
 
-
-import chromadb
-from chromadb.config import Settings
-from langchain.llms import HuggingFacePipeline
-from langchain.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Chroma
+from langchain.llms import CTransformers
+config = {'max_new_tokens': 100, 'temperature': 0}
+llm = CTransformers(model='TheBloke/Mistral-7B-Instruct-v0.1-GGUF',model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf", config=config)
 
 
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -64,32 +26,19 @@ def download(url):
 
 
 def summarize(transcript):
-          from langchain.schema.document import Document
-          documents = [Document(page_content=mna_news, metadata={"source": "local"})]
-          #######################
-          text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-          all_splits = text_splitter.split_documents(documents)
-          model_name = "sentence-transformers/all-mpnet-base-v2"
-          model_kwargs = {"device": "cuda"}
-          embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
-          #######################
-          vectordb = Chroma.from_documents(documents=all_splits, embedding=embeddings, persist_directory="chroma_db")
-          #######################
-          retriever = vectordb.as_retriever()
-          #######################
-          qa = RetrievalQA.from_chain_type(
-              llm=llm,
-              chain_type="stuff",
-              retriever=retriever,
-              verbose=True
-          )
-
-          
-          query = 'Summarize the text in 2-3 paragraphs'
-          result = qa.run(query)
-          print("\nResult: ", result)
-
-          return result
+        template = """<s>[INST] You are a helpful, respectful and honest assistant. Answer exactly in few words from the context
+        Answer the question below from context below :
+        {context}
+        {question} [/INST] </s>
+        """
+        
+        question_p = """Summarize the text in 2-3 paragraphs"""
+        context_p = transcript
+        prompt = PromptTemplate(template=template, input_variables=["question","context"])
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        response = llm_chain.run({"question":question_p,"context":context_p})
+        
+        return response
 
 
 
